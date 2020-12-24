488ca69e829474de56c119fa6170353a7e36b78a
xuan <gn0987123@yahoo.com.tw>
2020-12-24 12:53:18 +0800

    Initial Commit

---
+++ ComputerTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ComputerTest
+//
+//  Created by 陳暘璿 on 2020/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
